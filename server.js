44f440646081dd376b17d8935670a784361e5caa
Abdallah Abdelmoaty <abdallah.abdelmoaty@tasahel.local>
2024-09-01 13:48:45 +0300

    add airports

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const airportRoutes = require('./routes/airport');
+const airports = require('./airports')
 const port = process.env.PORT || 3000;
 
-app.use('/api',airportRoutes)
+app.get("/api/airports",(req,res)=>{
+    res.json(airports)
+});
 
 app.listen(port,()=>{
     console.log("is run");
