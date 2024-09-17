802938cd3b9039d872e1f530c5d4138912c932db
Tasahel <Abdullah.Abdelmouty@tasahel.com.eg>
2024-09-17 14:06:55 +0300

    add cors for tasahel

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const airportRoutes = require('./routes/airport');
 const airports = require('./airports')
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+var corsOptions = {
+    origin: 'https://tasahel.com.eg',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(cors(corsOptions));
 
 app.get("/api/airports",(req,res)=>{
     res.json(airports)
