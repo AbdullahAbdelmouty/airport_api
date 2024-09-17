2eb39a328a9a688ee60c5594972b25e355bc5cd4
Tasahel <Abdullah.Abdelmouty@tasahel.com.eg>
2024-09-17 13:51:07 +0300

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const airportRoutes = require('./routes/airport');
 const airports = require('./airports')
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/api/airports",(req,res)=>{
     res.json(airports)
