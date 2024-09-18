59f203d491f0aedbf931694ca2e63716df8c2aa1
Tasahel <Abdullah.Abdelmouty@tasahel.com.eg>
2024-09-18 12:00:02 +0300

    add 000

--- app.js
+++ app.js
@@ -5,26 +5,45 @@ const airportRoutes = require('./routes/airport');
 const airports = require('./airports')
 const port = process.env.PORT || 3000;
 
+// var corsOptions = {
+//     origin: ["https://tasahel.com.eg", "http://127.0.0.1:5500"],
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'If-Match', 'If-None-Match'] // Add other precondition headers if needed
+// };
+
 var corsOptions = {
     origin: ["https://tasahel.com.eg", "http://127.0.0.1:5500"],
     methods: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'If-Match', 'If-None-Match'] // Add other precondition headers if needed
+    allowedHeaders: ['Content-Type', 'Authorization']  // No need for If-Match/If-None-Match
 };
-
 
 
 
 
 // Apply CORS middleware
 app.use(cors(corsOptions));
+// app.options('*', (req, res) => {
+//     console.log('OPTIONS request headers:', req.headers);
+//     res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, If-Match, If-None-Match');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.sendStatus(204);  // Return OK for preflight request
+// });
+
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    console.log('OPTIONS request headers:', req.headers);
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, If-Match, If-None-Match');
-    res.sendStatus(200);  // Respond with 'No Content'
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Remove If-Match/If-None-Match
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(204);  // Return OK for preflight request
 });
-app.get("/api/airports",(req,res)=>{
+
+
+app.get("/api/airports",(req,res)=>{    
     res.json(airports);
 });
 
